6ebcd5593239a66bbc38075a685eddf7e100d790
SprengersH <86893159+SprengersH@users.noreply.github.com>
2022-05-18 11:02:33 +0200

    I got it, i think. Main creates a DatabaseController that gets a DAL that implements a Repository as a parameter. The controller has a Repository field and creates the entities, in this case Items.

---
+++ Domain/src/Interfaces/tests/ItemRepositoryTest.java
@@ -0,0 +1,25 @@
+package Interfaces.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ItemRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getItems() {
+    }
+
+    @Test
+    void selectMenu() {
+    }
+}
