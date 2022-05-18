1a43c2ab6c31afb6efb78ce595467e49d0b575ce
SprengersH <86893159+SprengersH@users.noreply.github.com>
2022-05-18 08:36:37 +0200

    main now creates an ItemDTO list that initiates the request from the DAL. It then passes the list to the DBcontroller in the domain-layer.

--- APresentation/src/UI/Main.java
+++ APresentation/src/UI/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<ItemDTO> itemList = new ItemDAL().getItems(); // cant put this in domain bc of dependency...
-        dbController.test(itemList); // now i have a list of ItemDTO's in the domain layer.
+        dbController.test(itemList); // now i have a list of ItemDTO's in the domain layer. Should this controller be in the presentation layer??
         run();
     }
 
